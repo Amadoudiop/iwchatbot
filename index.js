1356fcfd2a20c50a716f2a23553a2a33cf280d9c
Amadoudiop <d.amadoudiop@gmail.com>
2017-10-10 18:47:41 +0200

    TP2

---
+++ index.js
@@ -0,0 +1,146 @@
+var restify = require('restify')
+var builder = require('botbuilder')
+
+// setup the restify server
+var server = restify.createServer()
+server.listen(process.env.port || process.env.PORT || 3987, function(){
+	console.log('%s bot started at %s', server.name, server.url);
+})
+
+// create chat connector
+var connector = new builder.ChatConnector({
+	appId: process.env.APP_ID,
+	appPassword: process.env.APP_SECRET
+})
+
+// Listening for user inputs
+server.post('/api/messages', connector.listen())
+
+// Reply by echoing
+var bot = new builder.UniversalBot(connector,[
+    function(session){
+        //session.beginDialog('greetings', session);
+        // session.beginDialog('ensureProfile', session.userData.profile);
+        session.beginDialog('buyButtonClick', session);
+    }/*,
+    function(session, results) {
+        session.userData.profile = results.response; // Save user profile.
+        session.send(`Hello ${session.userData.profile.name}! I love ${session.userData.profile.company}!`);
+    }*/
+])
+
+
+bot.dialog('greetings', [
+    function (session) {
+        session.beginDialog('askName');
+    },
+    function (session, results) {
+        session.endDialog('Hello %s', results.response);
+    }
+]);
+
+bot.dialog('askName', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.endDialogWithResult(results);
+    }
+]);
+
+
+
+bot.dialog('ensureProfile', [
+    function (session, args, next) {
+        session.dialogData.profile = args || {}; // Set the profile or create the object.
+        if (!session.dialogData.profile.name) {
+            builder.Prompts.text(session, "What's your name?");
+        } else {
+            next(); // Skip if we already have this info.
+        }
+    },
+    function (session, results, next) {
+        if (results.response) {
+            // Save user's name if we asked for it.
+            session.dialogData.profile.name = results.response;
+        }
+        if (!session.dialogData.profile.company) {
+            builder.Prompts.text(session, "What company do you work for?");
+        } else {
+            next(); // Skip if we already have this info.
+        }
+    },
+    function (session, results) {
+        if (results.response) {
+            // Save company name if we asked for it.
+            session.dialogData.profile.company = results.response;
+        }
+        session.endDialogWithResult({ response: session.dialogData.profile });
+    }
+]);
+
+
+bot.dialog('dinnerOrder', [
+    //...waterfall steps...,
+    // Last step
+    function(session, results){
+        if(results.response){
+            session.dialogData.room = results.response;
+            var msg = `Thank you. Your order will be delivered to room #${session.dialogData.room}`;
+            session.endConversation(msg);
+        }
+    }
+])
+    .endConversationAction(
+        "endOrderDinner", "Ok. Goodbye.",
+        {
+            matches: /^cancel$|^goodbye$/i,
+            confirmPrompt: "This will cancel your order. Are you sure?"
+        }
+    );
+
+
+// Add dialog to handle 'Buy' button click
+bot.dialog('buyButtonClick', [
+    function (session, args, next) {
+        // Get color and optional size from users utterance
+        var utterance = args.intent.matched[0];
+        var color = /(white|gray)/i.exec(utterance);
+        var size = /\b(Extra Large|Large|Medium|Small)\b/i.exec(utterance);
+        if (color) {
+            // Initialize cart item
+            var item = session.dialogData.item = {
+                product: "classic " + color[0].toLowerCase() + " t-shirt",
+                size: size ? size[0].toLowerCase() : null,
+                price: 25.0,
+                qty: 1
+            };
+            if (!item.size) {
+                // Prompt for size
+                builder.Prompts.choice(session, "What size would you like?", "Small|Medium|Large|Extra Large");
+            } else {
+                //Skip to next waterfall step
+                next();
+            }
+        } else {
+            // Invalid product
+            session.send("I'm sorry... That product wasn't found.").endDialog();
+        }
+    },
+    function (session, results) {
+        // Save size if prompted
+        var item = session.dialogData.item;
+        if (results.response) {
+            item.size = results.response.entity.toLowerCase();
+        }
+
+        // Add to cart
+        if (!session.userData.cart) {
+            session.userData.cart = [];
+        }
+        session.userData.cart.push(item);
+
+        // Send confirmation to users
+        session.send("A '%(size)s %(product)s' has been added to your cart.", item).endDialog();
+    }
+]).triggerAction({ matches: /(buy|add)\s.*shirt/i });
